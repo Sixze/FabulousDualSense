dede0727837e42cef86e04e87e8608c73163735f
Sixze <16257871+Sixze@users.noreply.github.com>
2022-11-30 23:47:34 +0300

    Add BuildPlugin.bat

---
+++ BuildPlugin.bat
@@ -0,0 +1,15 @@
+@echo off
+
+set EngineVesion=5.0
+
+for /f "skip=2 tokens=2*" %%a in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\EpicGames\Unreal Engine\%EngineVesion%" /v "InstalledDirectory"') do set "EngineDirectory=%%b"
+
+set AutomationToolPath="%EngineDirectory%\Engine\Build\BatchFiles\RunUAT.bat"
+set PluginPath="%cd%\FabulousDualSense.uplugin"
+set OutputPath="%cd%\Build"
+
+echo Automation Tool Path: %AutomationToolPath%
+echo:
+
+call %AutomationToolPath% BuildPlugin -Plugin=%PluginPath% -Package=%OutputPath% -Rocket -TargetPlatforms=Win64
+pause
