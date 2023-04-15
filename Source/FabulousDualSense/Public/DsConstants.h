b7d03d31bb83feb744cd988cd6e634bcc2c11a24
Sixze <16257871+Sixze@users.noreply.github.com>
2023-04-15 18:58:59 +0300

    Review linkage and storage duration of global variables

--- Source/FabulousDualSense/Public/DsConstants.h
+++ Source/FabulousDualSense/Public/DsConstants.h
@@ -5,27 +5,27 @@
 
 namespace DsConstants
 {
-	inline static const FName InputDeviceName{TEXTVIEW("DsInputDevice")};
-	inline static const FString HardwareDeviceIdentifier{TEXTVIEW("DualSense")};
+	FABULOUSDUALSENSE_API extern const FName InputDeviceName;
+	FABULOUSDUALSENSE_API extern const FString HardwareDeviceIdentifier;
 
-	inline static constexpr auto MaxDevicesCount{4};
-	inline static constexpr auto ButtonsCount{29};
-	inline static constexpr auto StickDeadZone{30};
-	inline static constexpr auto TriggerDeadZone{30};
+	inline constexpr auto MaxDevicesCount{4};
+	inline constexpr auto ButtonsCount{29};
+	inline constexpr auto StickDeadZone{30};
+	inline constexpr auto TriggerDeadZone{30};
 
-	inline static const FKey TouchpadKey{FName{TEXTVIEW("DsTouchpad")}};
-	inline static const FKey LogoKey{FName{TEXTVIEW("DsLogo")}};
-	inline static const FKey MuteKey{FName{TEXTVIEW("DsMute")}};
+	FABULOUSDUALSENSE_API extern const FKey TouchpadKey;
+	FABULOUSDUALSENSE_API extern const FKey LogoKey;
+	FABULOUSDUALSENSE_API extern const FKey MuteKey;
 
-	inline static const FKey Touch1Key{FName{TEXTVIEW("DsTouch1")}};
-	inline static const FKey Touch1AxisXKey{FName{TEXTVIEW("DsTouch1AxisX")}};
-	inline static const FKey Touch1AxisYKey{FName{TEXTVIEW("DsTouch1AxisY")}};
-	inline static const FKey Touch1AxisXYKey{FName{TEXTVIEW("DsTouch1AxisXY")}};
+	FABULOUSDUALSENSE_API extern const FKey Touch1Key;
+	FABULOUSDUALSENSE_API extern const FKey Touch1AxisXKey;
+	FABULOUSDUALSENSE_API extern const FKey Touch1AxisYKey;
+	FABULOUSDUALSENSE_API extern const FKey Touch1AxisXYKey;
 
-	inline static const FKey Touch2Key{FName{TEXTVIEW("DsTouch2")}};
-	inline static const FKey Touch2AxisXKey{FName{TEXTVIEW("DsTouch2AxisX")}};
-	inline static const FKey Touch2AxisYKey{FName{TEXTVIEW("DsTouch2AxisY")}};
-	inline static const FKey Touch2AxisXYKey{FName{TEXTVIEW("DsTouch2AxisXY")}};
+	FABULOUSDUALSENSE_API extern const FKey Touch2Key;
+	FABULOUSDUALSENSE_API extern const FKey Touch2AxisXKey;
+	FABULOUSDUALSENSE_API extern const FKey Touch2AxisYKey;
+	FABULOUSDUALSENSE_API extern const FKey Touch2AxisXYKey;
 
 	FABULOUSDUALSENSE_API const TMap<FGamepadKeyNames::Type, uint32>& GetRegularButtons();
 }
